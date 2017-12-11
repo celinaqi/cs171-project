270c0361ad8cc2f8433f7f5a8a88e218e0f55085
michelleng <michelleng@college.harvard.edu>
2017-12-10 19:11:31 -0500

    took out stupid treemap

--- assets/js/water-calculator.js
+++ assets/js/water-calculator.js
@@ -95,8 +95,10 @@ function updateVisualization(shower, flush, runningWater, laundry, dishes, drink
     var vis = this;
 
     // delete previous "You" data
-    studentData.forEach(function(d, i) {
-        if (d.student === "You") {studentData.splice(i, 1)}
+    studentData.forEach(function (d, i) {
+        if (d.student === "You") {
+            studentData.splice(i, 1)
+        }
     });
 
     studentData.push({
@@ -110,9 +112,13 @@ function updateVisualization(shower, flush, runningWater, laundry, dishes, drink
         return b.consumption - a.consumption;
     });
 
-    vis.x1.domain(studentData.map(function (d) {return d.student}));
+    vis.x1.domain(studentData.map(function (d) {
+        return d.student
+    }));
 
-    vis.y1.domain([0, d3.max(studentData, function (d) {return d.consumption})]);
+    vis.y1.domain([0, d3.max(studentData, function (d) {
+        return d.consumption
+    })]);
 
     // Draw bar chart
     vis.bars1 = vis.svgCalculator1.selectAll(".bar")
@@ -133,9 +139,14 @@ function updateVisualization(shower, flush, runningWater, laundry, dishes, drink
             return height1 - vis.y1(d.consumption);
         })
         .attr("width", vis.x1.bandwidth() - 10)
-        .attr("fill", function(d) {
-            if (d.student === "You") {return d3.rgb("#ed4933")}
-            else {return "steelblue"}})
+        .attr("fill", function (d) {
+            if (d.student === "You") {
+                return d3.rgb("#ed4933")
+            }
+            else {
+                return "steelblue"
+            }
+        })
 
     vis.bars1.exit().remove();
 
@@ -167,7 +178,7 @@ function updateVisualization(shower, flush, runningWater, laundry, dishes, drink
     vis.bartitle = vis.svgCalculator1
         .append("text")
         .attr("class", "bartitle")
-        .attr("x", width1/5)
+        .attr("x", width1 / 5)
         .attr("y", -10)
         .text("Consumption of Water (Gallons per Day)");
 
@@ -177,189 +188,4 @@ function updateVisualization(shower, flush, runningWater, laundry, dishes, drink
     vis.svgCalculator1.select(".x-axis")
         .transition()
         .call(vis.xAxis1);
-
-    var treeMap =
-        // {
-        //     "name": "map",
-        //     "children": [
-        //         {"name": "shower", "size": 10},
-        //         {"name": "flush", "size": 15},
-        //         {"name": "sink", "size": 5},
-        //         {"name": "laundry", "size": 6},
-        //         {"name": "dishes", "size": 3},
-        //         {"name": "drinking", "size": 8},
-        //         {"name": "driving", "size": 100}
-        //     ]
-        // };
-        {
-            "name": "Map",
-            "children": [
-                {
-                    "name": "Johanna",
-                    "children": [
-                        {"name": "shower", "size": 16},
-                        {"name": "flush", "size": 16},
-                        {"name": "sink", "size": 4},
-                        {"name": "laundry", "size": 0.80},
-                        {"name": "dishes", "size": 3.43},
-                        {"name": "drinking", "size": 0.56},
-                        {"name": "driving", "size": 20}
-                        ]
-                }
-                ]
-        };
-        //         {
-        //             "name": "Ronell",
-        //             "children": [
-        //                 {"name": "shower", "size": 40},
-        //                 {"name": "flush", "size": 20},
-        //                 {"name": "sink", "size": 30},
-        //                 {"name": "laundry", "size": 1.60},
-        //                 {"name": "dishes", "size": 6},
-        //                 {"name": "drinking", "size": 0.63},
-        //                 {"name": "driving", "size": 10}
-        //             ]
-        //         },
-        //         {
-        //             "name": "Zona",
-        //             "children": [
-        //                 {"name": "shower", "size": 60},
-        //                 {"name": "flush", "size": 10},
-        //                 {"name": "sink", "size": 30},
-        //                 {"name": "laundry", "size": 1.60},
-        //                 {"name": "dishes", "size": 6},
-        //                 {"name": "drinking", "size": 0.63},
-        //                 {"name": "driving", "size": 210}
-        //             ]
-        //         },
-        //         {
-        //             "name": "Fritz",
-        //             "children": [
-        //                 {"name": "shower", "size": 20},
-        //                 {"name": "flush", "size": 10},
-        //                 {"name": "sink", "size": 6},
-        //                 {"name": "laundry", "size": 0.27},
-        //                 {"name": "dishes", "size": 8.57},
-        //                 {"name": "drinking", "size": 0.25},
-        //                 {"name": "driving", "size": 0}
-        //             ]
-        //         },
-        //         {
-        //             "name": "You",
-        //             "children": [
-        //                 {"name": "shower", "size": shower},
-        //                 {"name": "flush", "size": flush},
-        //                 {"name": "sink", "size": runningWater},
-        //                 {"name": "laundry", "size": laundry.toFixed(2)},
-        //                 {"name": "dishes", "size": dishes},
-        //                 {"name": "drinking", "size": drinks.toFixed(2)},
-        //                 {"name": "driving", "size": drives}
-        //             ]
-        //         }
-        //     ]
-        // };
-
-        // { "name": "shower",
-        //     "children": [
-        //         {"name": "shower", "size": 10}
-        //         ]
-        //  },
-        // { "name": "flush",
-        //     "children": [
-        //         {"name": "flush", "size": 15}
-        //     ]
-        // };
-        //     {"name": "shower", "size": 10};
-        //     {"name": "flush", "size": 15};
-        //     {"name": "sink", "size": 5};
-        //     {"name": "laundry", "size": 6};
-        //     {"name": "dishes", "size": 3};
-        //     {"name": "drinking", "size": 8};
-        //     {"name": "driving", "size": 100};
-
-    const margin = {top: 40, right: 40, bottom: 10, left: 50},
-        width = $('#water-chart1-treemap').width() - margin.left - margin.right,
-        height = 300 - margin.top - margin.bottom,
-        color = d3.scaleOrdinal().range(d3.schemeCategory20c);
-
-    const treemap = d3.treemap().size([width, height]);
-
-    const div = d3.select("#water-chart1-treemap").append("div")
-        .style("position", "relative")
-        .style("width", (width + margin.left + margin.right) + "px")
-        .style("height", (height + margin.top + margin.bottom) + "px")
-        .style("left", margin.left + "px")
-        .style("top", margin.top + "px");
-
-    //get data
-    const root = d3.hierarchy(treeMap, (d) => d.children)
-        .sum((d) => d.size);
-
-        const tree = treemap(root);
-
-        // Tooltip attempts
-    // var divToolTip = d3.select("#water-chart1-treemap").append("div")
-    //     .attr("class", "tooltip")
-    //     .style("opacity", 0);
-    //
-    // var mousemove = function(d) {
-    //     var xPosition = d3.event.pageX + 5;
-    //     var yPosition = d3.event.pageY + 5;
-    //
-    //     d3.select("#tooltip")
-    //         .style("left", xPosition + "px")
-    //         .style("top", yPosition + "px");
-    //     d3.select("#tooltip")
-    //         .text(d.data.name + "<br/>" + d.data.size);
-    //     d3.select("#tooltip").classed("hidden", false);
-    // };
-    //
-    // var mouseout = function() {
-    //     d3.select("#tooltip").classed("hidden", true);
-    // };
-
-        const treeMapNode = div.datum(root).selectAll(".treeMapNode")
-            .data(tree.leaves())
-            .enter().append("div")
-            .attr("class", "treeMapNode")
-            .style("left", (d) => d.x0 + "px")
-    .style("top", (d) => d.y0 + "px")
-    .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-    .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
-    .style("background", (d) => color(d.parent.data.name))
-    .text((d) => d.data.name + ": " + d.data.size);
-    // .on("mousemove", mousemove)
-    //     .on("mouseout", mouseout);
-
-
-    d3.selectAll("input").on("change", function change() {
-            const value = this.value === "count"
-                ? (d) => { return d.size ? 1 : 0;}
-        : (d) => { return d.size; };
-
-            const newRoot = d3.hierarchy(treeMap, (d) => d.children)
-        .sum(value);
-
-            treeMapNode.data(treemap(newRoot).leaves())
-                .transition()
-                .duration(1500)
-                .style("left", (d) => d.x0 + "px")
-        .style("top", (d) => d.y0 + "px")
-        .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-        .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
-        });
-        // .on("mouseover", function(d) {
-        //     div.transition()
-        //         .duration(200)
-        //         .style("opacity", .9);
-        //     div.html(d.data.name + "<br/>" + d.data.size)
-        //         .style("left", (d3.event.pageX) + "px")
-        //         .style("top", (d3.event.pageY - 28) + "px");
-        // })
-        // .on("mouseout", function(d) {
-        //     div.transition()
-        //         .duration(500)
-        //         .style("opacity", 0);
-        // });
-
 }
